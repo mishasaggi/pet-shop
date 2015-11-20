7d8e728ca7b72cd1e4079ec85b345d786136a082
Gilbert <gilbertbgarza@gmail.com>
2015-11-20 09:41:01 -0600

    Extend m.request with .catch

--- ext/index.js
+++ ext/index.js
@@ -2,3 +2,16 @@ require('./arrays');
 require('./functions');
 require('./globals');
 require('./objects');
+
+//
+// Extend m.request to make it behave more like a promise.
+// This change is in the next version of mithril.
+//
+var m = require('mithril');
+
+var original = m.request;
+m.request = function () {
+  var result = original.apply(m, arguments);
+  result.catch = result.then.bind(result, null);
+  return result;
+}
