f673c3617e7d9d1cffe2ed2a3f1069aae3e8efa1
Gilbert <gilbertbgarza@gmail.com>
2015-11-20 09:54:04 -0600

    Simplify server code

--- server/index.js
+++ server/index.js
@@ -3,20 +3,21 @@ var browserify = require('browserify-middleware');
 var express = require('express');
 var app = express();
 
-//provide a browserified file at a path
-var shared = ['mithril'];
-
-app.get('/scripts/vendor-bundle.js',
-  browserify(shared));
-
+//
+// Serve a browserified file for GET /scripts/app-bundle.js
+//
 app.get('/scripts/app-bundle.js',
-  browserify('./client/main.js', { external: shared }));
+  browserify('./client/main.js'));
 
-// Serve sass file
+//
+// Serve sass file for GET /styles/main.css
+//
 var sass = require('node-sass-endpoint');
 app.get('/styles/main.css', sass.serve('./client/main.scss'));
 
+//
 // Non-js static files
+//
 app.use(express.static('client/public'));
 
 var port = process.env.PORT || 4000;
